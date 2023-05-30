029ea870f429be46a125490cbe2f48d75de03502
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-05-29 23:58:35 -0300

    Update category

--- src/main/java/com/botcamp/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/botcamp/dscatalog/resources/CategoryResource.java
@@ -37,4 +37,10 @@ public class CategoryResource {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+        dto = categoryService.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
 }
--- src/main/java/com/botcamp/dscatalog/service/CategoryService.java
+++ src/main/java/com/botcamp/dscatalog/service/CategoryService.java
@@ -4,6 +4,7 @@ import com.botcamp.dscatalog.dto.CategoryDTO;
 import com.botcamp.dscatalog.entities.Category;
 import com.botcamp.dscatalog.repositories.CategoryRepository;
 import com.botcamp.dscatalog.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,23 +16,35 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
-    
+
     @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id){
+    public CategoryDTO findById(Long id) {
         return new CategoryDTO((categoryRepository.findById(id).
-                orElseThrow(() -> new ResourceNotFoundException("Objeto não econtrado")) ));
+                orElseThrow(() -> new ResourceNotFoundException("Objeto não econtrado"))));
     }
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
+    public List<CategoryDTO> findAll() {
         List<Category> lista = categoryRepository.findAll();
-        return  lista.stream().map(x -> new CategoryDTO(x)).toList();
+        return lista.stream().map(x -> new CategoryDTO(x)).toList();
     }
 
     @Transactional(readOnly = true)
-    public CategoryDTO save(CategoryDTO dto){
+    public CategoryDTO save(CategoryDTO dto) {
         Category obj = new Category();
         obj.setName(dto.getName());
-       return new CategoryDTO(categoryRepository.save(obj));
+        return new CategoryDTO(categoryRepository.save(obj));
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            //Instancia um novo objeto, porém não irá acessar o banco de dados
+            Category obj = categoryRepository.getReferenceById(id);
+            obj.setName(dto.getName());
+            return new CategoryDTO(categoryRepository.save(obj));
+        } catch (EntityNotFoundException e) {
+            throw  new ResourceNotFoundException("Id not found" + id);
+        }
     }
 }
